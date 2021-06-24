abb3f66caff7908c382de251707aade4affe4027
Syed Zayyan <masudzayyan@gmail.com>
2021-06-24 08:32:41 +0100

    added weather icons

--- main/main.py
+++ main/main.py
@@ -10,6 +10,11 @@ sys.path.append("..")
 from lib import LCD_1inch14
 from PIL import Image,ImageDraw,ImageFont
 from gpiozero import CPUTemperature
+import glob
+
+
+
+
 
 # Raspberry Pi pin configuration:
 RST = 27
@@ -18,6 +23,27 @@ BL = 18
 bus = 0 
 device = 0 
 logging.basicConfig(level=logging.DEBUG)
+
+
+icon_map = {
+    "snow": ["snow", "sleet"],
+    "rain": ["rain"],
+    "cloud": ["fog", "cloudy", "partly-cloudy-day", "partly-cloudy-night"],
+    "sun": ["clear-day", "clear-night"],
+    "storm": [],
+    "wind": ["wind"]
+}
+
+# Pre-load icons into a dictionary with PIL
+icons = {}
+
+for icon in glob.glob("../pic/icons/*.png"):
+    icon_name = icon.split("/")[1].replace(".png", "")
+    icon_image = Image.open(icon)
+    icons[icon_name] = icon_image
+
+
+
 try:
     # display with hardware SPI:
     ''' Warning!!!Don't  creation of multiple displayer objects!!! '''
@@ -35,12 +61,15 @@ try:
     image2 = Image.new("RGB", (disp.width, disp.height), "WHITE")
     draw = ImageDraw.Draw(image2)
 
+    draw.paste(icons["cloud"], (10, 46))
+
     cpu = CPUTemperature()
     temp = "CPU Temp: " + str(cpu.temperature)
     logging.info("draw text")
     draw.text((0, 45), temp, font = Font0, fill = "BLACK")
     disp.ShowImage(image2)
     time.sleep(5)
+
     
     disp.module_exit()
     logging.info("quit:")
