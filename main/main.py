ffac3e2961acbd88d47bfc145fd2e517c47947e0
syedzayyan <masudzayyan@gmail.com>
2021-06-24 12:52:40 +0100

    got all things done nearly

--- main/main.py
+++ main/main.py
@@ -12,6 +12,13 @@ from PIL import Image,ImageDraw,ImageFont
 from gpiozero import CPUTemperature
 import glob
 import requests, json
+from dotenv import load_dotenv
+from datetime import datetime
+
+load_dotenv()
+
+
+
 
 # Raspberry Pi pin configuration:
 RST = 27
@@ -21,19 +28,18 @@ bus = 0
 device = 0 
 logging.basicConfig(level=logging.DEBUG)
 
-URL = "http://api.openweathermap.org/data/2.5/weather?q=London&appid=" + os.environ["API_KEY"]
+URL = "http://api.openweathermap.org/data/2.5/weather?q=London&appid=" + os.getenv("API_KEY")
 
 # Pre-load icons into a dictionary with PIL
 icons = {}
 
-for icon in glob.glob("../pic/*.png"):
-    icon_name = icon.split("/")[1].replace(".png", "")
-    icon_image = Image.open(icon)
-    icons[icon_name] = icon_image
-
 response = requests.get(URL)
 data = response.json()
-print(data)
+currTemp = data["main"]["temp"]
+feelsLike = data["main"]["feels_like"]
+minTemp = data["main"]["temp_min"]
+maxTemp = data["main"]["temp_max"]
+tempIcon = data["weather"][0]["icon"]
 
 try:
     # display with hardware SPI:
@@ -47,17 +53,30 @@ try:
     
     # Create blank image for drawing.
     
-    Font0 = ImageFont.truetype("../Font/Font00.ttf",20)
-    
+    Font0 = ImageFont.truetype("../Font/Font02.ttf",18)
+    Font1 = ImageFont.truetype("../Font/Font00.ttf",35)
+
     image2 = Image.new("RGB", (disp.width, disp.height), "WHITE")
     draw = ImageDraw.Draw(image2)
+    picDir = "../pic/" + tempIcon + ".png"
+    weatherIcon = Image.open(picDir)
+    image2.paste(weatherIcon, (0, 15))
 
-    image2.paste(icons["01d"], (10, 46))
+    feelText = "Feels Like: " + str(int(feelsLike -273.00)) + "C" 
+    draw.text((0, 15), feelText, font = Font0, fill = "BLACK")
+
+    minMax = "Max Temp: " + str(int(minTemp - 273.00)) + "C || Min Temp: " + str(int(maxTemp-273.00)) + "C"
+    draw.text((0, 30), minMax, font = Font0, fill = "BLACK")
+
+
+    currTempText = str(int(currTemp - 273.00)) + "C"
+    draw.text((130, 50), currTempText, font = Font1, fill = "BLACK")
+    draw.text((140, 100), datetime.today().strftime('%Y-%m-%d'), font = Font0, fill = "BLUE")
 
     cpu = CPUTemperature()
-    temp = "CPU Temp: " + str(cpu.temperature)
+    temp = "CPU Temp: " + str(cpu.temperature) + "C"
     logging.info("draw text")
-    draw.text((0, 45), temp, font = Font0, fill = "BLACK")
+    draw.text((0, 0), temp, font = Font0, fill = "BLACK")
     disp.ShowImage(image2)
     time.sleep(5)
 
