54a8dd7aa9cec4283477a8f9b3b1acc28f36fc8b
Syed Zayyan <masudzayyan@gmail.com>
2021-06-24 12:56:42 +0100

    maybe done :)

--- main/main.py
+++ main/main.py
@@ -17,9 +17,6 @@ from datetime import datetime
 
 load_dotenv()
 
-
-
-
 # Raspberry Pi pin configuration:
 RST = 27
 DC = 25
@@ -27,65 +24,61 @@ BL = 18
 bus = 0 
 device = 0 
 logging.basicConfig(level=logging.DEBUG)
+while True:
+    try:
+        URL = "http://api.openweathermap.org/data/2.5/weather?q=London&appid=" + os.getenv("API_KEY")
 
-URL = "http://api.openweathermap.org/data/2.5/weather?q=London&appid=" + os.getenv("API_KEY")
+        response = requests.get(URL)
+        data = response.json()
+        currTemp = data["main"]["temp"]
+        feelsLike = data["main"]["feels_like"]
+        minTemp = data["main"]["temp_min"]
+        maxTemp = data["main"]["temp_max"]
+        tempIcon = data["weather"][0]["icon"]
+        # display with hardware SPI:
+        ''' Warning!!!Don't  creation of multiple displayer objects!!! '''
+        #disp = LCD_1inch14.LCD_1inch14(spi=SPI.SpiDev(bus, device),spi_freq=10000000,rst=RST,dc=DC,bl=BL)
+        disp = LCD_1inch14.LCD_1inch14()
+        # Initialize library.
+        disp.Init()
+        # Clear display.
+        disp.clear()
+        
+        # Create blank image for drawing.
+        
+        Font0 = ImageFont.truetype("../Font/Font02.ttf",18)
+        Font1 = ImageFont.truetype("../Font/Font00.ttf",35)
 
-# Pre-load icons into a dictionary with PIL
-icons = {}
+        image2 = Image.new("RGB", (disp.width, disp.height), "WHITE")
+        draw = ImageDraw.Draw(image2)
+        picDir = "../pic/" + tempIcon + ".png"
+        weatherIcon = Image.open(picDir)
+        image2.paste(weatherIcon, (0, 15))
 
-response = requests.get(URL)
-data = response.json()
-currTemp = data["main"]["temp"]
-feelsLike = data["main"]["feels_like"]
-minTemp = data["main"]["temp_min"]
-maxTemp = data["main"]["temp_max"]
-tempIcon = data["weather"][0]["icon"]
+        feelText = "Feels Like: " + str(int(feelsLike -273.00)) + "C" 
+        draw.text((0, 15), feelText, font = Font0, fill = "BLACK")
 
-try:
-    # display with hardware SPI:
-    ''' Warning!!!Don't  creation of multiple displayer objects!!! '''
-    #disp = LCD_1inch14.LCD_1inch14(spi=SPI.SpiDev(bus, device),spi_freq=10000000,rst=RST,dc=DC,bl=BL)
-    disp = LCD_1inch14.LCD_1inch14()
-    # Initialize library.
-    disp.Init()
-    # Clear display.
-    disp.clear()
-    
-    # Create blank image for drawing.
-    
-    Font0 = ImageFont.truetype("../Font/Font02.ttf",18)
-    Font1 = ImageFont.truetype("../Font/Font00.ttf",35)
-
-    image2 = Image.new("RGB", (disp.width, disp.height), "WHITE")
-    draw = ImageDraw.Draw(image2)
-    picDir = "../pic/" + tempIcon + ".png"
-    weatherIcon = Image.open(picDir)
-    image2.paste(weatherIcon, (0, 15))
-
-    feelText = "Feels Like: " + str(int(feelsLike -273.00)) + "C" 
-    draw.text((0, 15), feelText, font = Font0, fill = "BLACK")
-
-    minMax = "Max Temp: " + str(int(minTemp - 273.00)) + "C || Min Temp: " + str(int(maxTemp-273.00)) + "C"
-    draw.text((0, 30), minMax, font = Font0, fill = "BLACK")
+        minMax = "Max Temp: " + str(int(minTemp - 273.00)) + "C || Min Temp: " + str(int(maxTemp-273.00)) + "C"
+        draw.text((0, 30), minMax, font = Font0, fill = "BLACK")
 
 
-    currTempText = str(int(currTemp - 273.00)) + "C"
-    draw.text((130, 50), currTempText, font = Font1, fill = "BLACK")
-    draw.text((140, 100), datetime.today().strftime('%Y-%m-%d'), font = Font0, fill = "BLUE")
+        currTempText = str(int(currTemp - 273.00)) + "C"
+        draw.text((130, 50), currTempText, font = Font1, fill = "BLACK")
+        draw.text((140, 100), datetime.today().strftime('%Y-%m-%d'), font = Font0, fill = "BLUE")
 
-    cpu = CPUTemperature()
-    temp = "CPU Temp: " + str(cpu.temperature) + "C"
-    logging.info("draw text")
-    draw.text((0, 0), temp, font = Font0, fill = "BLACK")
-    disp.ShowImage(image2)
-    time.sleep(5)
+        cpu = CPUTemperature()
+        temp = "CPU Temp: " + str(cpu.temperature) + "C"
+        logging.info("draw text")
+        draw.text((0, 0), temp, font = Font0, fill = "BLACK")
+        disp.ShowImage(image2)
+        time.sleep(7200)
 
-    
-    disp.module_exit()
-    logging.info("quit:")
-except IOError as e:
-    logging.info(e)    
-except KeyboardInterrupt:
-    disp.module_exit()
-    logging.info("quit:")
-    exit()
+        
+        disp.module_exit()
+        logging.info("quit:")
+    except IOError as e:
+        logging.info(e)    
+    except KeyboardInterrupt:
+        disp.module_exit()
+        logging.info("quit:")
+        exit()
